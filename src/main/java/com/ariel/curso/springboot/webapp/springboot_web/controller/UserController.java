c363eaac02c598efb5a52adb43a9d503e4f0f347
Ariel Paricagua <arielfparicaguav@gmail.com>
2025-12-29 11:03:00 -0400

    feature: mi primer controlador manejando desde SpringBoot

---
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.ariel.curso.springboot.webapp.springboot_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/details")
+    public String details() {
+        return "details";
+    }
+}
