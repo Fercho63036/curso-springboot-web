0261a94c9ea6303bc08e4824c7d410e414e08239
Ariel Paricagua <arielfparicaguav@gmail.com>
2025-12-29 11:36:19 -0400

    Featurers: Pasar datos a la vista con model

--- src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/UserController.java
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.ariel.curso.springboot.webapp.springboot_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name", "Ariel");
+        model.addAttribute("lastname", "Paricagua");
+
         return "details";
     }
+
 }
