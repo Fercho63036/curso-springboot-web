bc49b16abb2b2f096eef7ac55b1435b9c997def4
Fercho63036 <arielfparicaguav@gmail.com>
2026-03-24 15:11:36 -0400

    Feature: Obtener varios parámetros de la URL con @RequestParam

--- src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
@@ -6,17 +6,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamDto;
+import com.ariel.curso.springboot.webapp.springboot_web.models.ParamMixDto;
 
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController {
 
     @GetMapping("/foo")
-    public ParamDto foo(@RequestParam(required = false, defaultValue = "Hola, No hay nada", name= "mensaje") String message) {
+    public ParamDto foo(@RequestParam(required = false, defaultValue = "Hola, No hay nada") String message) {
         ParamDto paramDto = new ParamDto();
         paramDto.setMessage(message);
         return paramDto;
     }
     // La manera de usar de pasar parmetros es con la URL: http://localhost:8080/api/params/foo?message=Hola%20Mundo
 
+    @GetMapping("/bar")
+    public ParamMixDto bar(@RequestParam String text, @RequestParam Integer code) {
+
+        ParamMixDto paramDto = new ParamMixDto();
+        paramDto.setMessage(text);
+        paramDto.setCode(code);
+        return paramDto;
+    
+    }
+    // La manera de usar de pasar parmetros es con la URL: http://localhost:8080/api/params/bar?text=Hola%20Mundo&code=200
 }
