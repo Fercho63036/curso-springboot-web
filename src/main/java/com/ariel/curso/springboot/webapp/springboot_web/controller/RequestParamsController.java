d968c13211df57259bbb137b1bf6e71f11e21925
Fercho63036 <arielfparicaguav@gmail.com>
2026-03-24 16:09:43 -0400

    Feature: dIFerencia de manejo de captaras parametros y request

--- src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
@@ -1,9 +1,12 @@
 package com.ariel.curso.springboot.webapp.springboot_web.controller;
 
+import javax.swing.Spring;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamDto;
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamMixDto;
@@ -32,6 +35,7 @@ public class RequestParamsController {
     }
 
     // La manera de usar de pasar parmetros es con la URL: http://localhost:8080/api/params/bar?text=Hola%20Mundo&code=200
+    // @RequestParam: Spring obtiene y convierte los parámetros automáticamente → más simple y limpio.
 
     @GetMapping("/request")
     public ParamMixDto request(HttpServletRequest request) {
@@ -48,5 +52,5 @@ public class RequestParamsController {
     }
     // Está validando manualmente los parámetros de la URL y asegurando que code siempre tenga un valor válido (aunque sea por defecto).
     // La manera de usar de pasar parmetros es con la URL: http://localhost:8080/api/params/request?code=1234567890&message=algun%20mensaje
-
+    // HttpServletRequest: tú obtienes y conviertes los parámetros manualmente → más control pero más código.
 }
