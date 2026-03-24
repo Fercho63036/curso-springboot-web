e7a81ea16cdaa223d43df654f62fe1f7b8fd57b1
Fercho63036 <arielfparicaguav@gmail.com>
2026-03-24 16:34:12 -0400

    Feature

---
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/PathVariableController.java
@@ -0,0 +1,23 @@
+package com.ariel.curso.springboot.webapp.springboot_web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ariel.curso.springboot.webapp.springboot_web.models.ParamDto;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+    // Este controlador toma un valor desde la URL (ruta), lo convierte en variable y lo devuelve en un objeto.
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+        ParamDto params = new ParamDto();
+        params.setMessage(message);
+        return params;
+    }
+    // @RequestParam: toma datos desde la URL (?param=valor) de forma automática.
+    // http://localhost:8080/api/var/baz/hola%20este%20es%20un%20mensaje%20muy%20importante
+}
--- src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/RequestParamsController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamDto;
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamMixDto;
