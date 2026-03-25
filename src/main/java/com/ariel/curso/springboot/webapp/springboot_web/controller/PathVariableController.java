ddf93e895db8566461bcec36a0f4841aad9ce8a0
Fercho63036 <arielfparicaguav@gmail.com>
2026-03-25 10:42:53 -0400

    Feature: Inyectar valores usando la anotacion @values

--- src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/PathVariableController.java
+++ src/main/java/com/ariel/curso/springboot/webapp/springboot_web/controller/PathVariableController.java
@@ -3,6 +3,7 @@ package com.ariel.curso.springboot.webapp.springboot_web.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ariel.curso.springboot.webapp.springboot_web.models.ParamDto;
 import com.ariel.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    // Trae un valor del properties y lo guarda en una variable
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
     // baz → 1 dato → devuelve un objeto (DTO)
     // Toma un valor desde la URL (path), lo convierte en variable y lo retorna en un objeto.
     // http://localhost:8080/api/var/baz/hola%20este%20es%20un%20mensaje%20muy%20importante
@@ -38,9 +52,24 @@ public class PathVariableController {
         return json;
     }
 
+    // Paso por cuerpo del objeto (JSON) y lo devuelve transformado (DTO)
     @PostMapping("/create")
     public User create(@RequestBody User user) {
         // user.setName(user.getName().toUpperCase());
         return user;
     }
+
+    // Manejo del controlador para pasar datos desde application.properties y devolverlos en un JSON
+    @GetMapping("/values")
+    public Map<String, Object> values( @Value("${config.message}") String message ) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return json;
+
+    }
+    
 }
